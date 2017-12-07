e55493dfe9e4d19876a6a137afd56d5e44bf3048
ThalisonBauer <thalisonbauer@hotmail.com>
2017-12-06 22:58:13 -0200

    Oe :( The end

--- app/src/main/java/com/example/thali/primeiroprojeto/MainActivity.java
+++ app/src/main/java/com/example/thali/primeiroprojeto/MainActivity.java
@@ -98,7 +98,6 @@ public class MainActivity extends AppCompatActivity {
                                             break;
                                     }
                                     break;
-
                             }
                         }else{
                             msn();
